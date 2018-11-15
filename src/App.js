400c054f8acc715849f6276f6c43fbe66e07b282
Nickolay Pelov <nikolaypelov@gmail.com>
2018-11-15 17:45:52 +0100

    Prettier test

--- src/App.js
+++ src/App.js
@@ -3,26 +3,98 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		const i = [
+			1,
+			2,
+			3,
+			4,
+			4,
+			5,
+			56,
+			3,
+			4,
+			3,
+			3,
+			2,
+			3,
+			4,
+			4,
+			5,
+			56,
+			3,
+			4,
+			3,
+			3,
+			2,
+			3,
+			4,
+			4,
+			5,
+			56,
+			3,
+			4,
+			3,
+			3,
+			2,
+			3,
+			4,
+			4,
+			5,
+			56,
+			3,
+			4,
+			3,
+			3,
+			2,
+			3,
+			4,
+			4,
+			5,
+			56,
+			3,
+			4,
+			3,
+			3,
+			2,
+			3,
+			4,
+			4,
+			5,
+			56,
+			3,
+			4,
+			3,
+			3,
+			2,
+			3,
+			4,
+			4,
+			5,
+			56,
+			3,
+			4,
+			3,
+			3,
+		];
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.js</code> and save to reload.
+					</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer">
+						Learn React
+					</a>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
